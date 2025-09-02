3b59dafb2463b3005d80b5b3803e41b0e561079a
kazadijacques <kazadijacques100@gmail.com>
2025-09-02 11:01:20 +0200

    convert products into classes

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -2,6 +2,7 @@ import {cart, addToCart} from '../data/cart.js';
 import {products} from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 
+
 let productsHTML = '';
 
 products.forEach((product) => {
@@ -19,14 +20,14 @@ products.forEach((product) => {
 
             <div class="product-rating-container">
               <img class="product-rating-stars"
-                src="images/ratings/rating-${product.rating.stars * 10}.png">
+                src="${product.getStarsUrl()}">
               <div class="product-rating-count link-primary">
                 ${product.rating.count}
               </div>
             </div>
 
             <div class="product-price">
-              $${formatCurrency(product.priceCents)}
+              ${product.getPrice()}
             </div>
 
             <div class="product-quantity-container">
@@ -43,6 +44,8 @@ products.forEach((product) => {
                 <option value="10">10</option>
               </select>
             </div>
+
+            ${product.extraInfoHTML()}
 
             <div class="product-spacer"></div>
 
